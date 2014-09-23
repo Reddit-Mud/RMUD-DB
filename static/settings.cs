ca1fdad0e2ae0b222c0f81fd92cf4519125eef60
Blecki <jm@omnisu.com>
2014-09-22 23:25:44 -0400

    Copy from RMUD repository

---
+++ static/settings.cs
@@ -0,0 +1,8 @@
+﻿public class settings : RMUD.Settings
+{
+	public override void Initialize()
+	{
+		Banner = "~~== REDDIT MUD ==~~\r\n";
+		MessageOfTheDay = "Prototype by Blecki\r\nFor now, just type 'login [name]'. You'll enter the world and be given wizard powers. Neat!\r\n";
+	}
+}
