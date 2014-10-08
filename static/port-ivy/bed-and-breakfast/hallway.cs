2e042421f886a01daffed89ec6e48113ae4e3778
Blecki <jm@omnisu.com>
2014-10-08 13:22:28 -0400

    More bed and breakfast

--- static/port-ivy/bed-and-breakfast/hallway.cs
+++ static/port-ivy/bed-and-breakfast/hallway.cs
@@ -1,4 +1,6 @@
-﻿class hallway : RMUD.Room
+﻿//import scenery_lamp
+
+class hallway : RMUD.Room
 {
     public override void Initialize()
     {
@@ -7,7 +9,9 @@
 
         OpenLink(RMUD.Direction.DOWN, "port-ivy/bed-and-breakfast/sitting_room");
         OpenLink(RMUD.Direction.NORTH, "port-ivy/bed-and-breakfast/bay_room");
+        OpenLink(RMUD.Direction.UP, "port-ivy/bed-and-breakfast/landing");
         
+        AddScenery(new scenery_lamp("wall sconse", "A little sconse on the wall is enough to fill the hallway with light."));
     }
 }
 
---
+++ static/port-ivy/bed-and-breakfast/landing.cs
@@ -0,0 +1,17 @@
+﻿//import scenery_lamp
+
+class landing : RMUD.Room
+{
+    public override void Initialize()
+    {
+        RoomType = RMUD.RoomType.Interior;
+        Short = "Port Ivy - Bed and Breakfast - Landing";
+
+        OpenLink(RMUD.Direction.EAST, "port-ivy/bed-and-breakfast/henrietta_wok_room");
+        OpenLink(RMUD.Direction.NORTH, "port-ivy/bed-and-breakfast/closet");
+        OpenLink(RMUD.Direction.DOWN, "port-ivy/bed-and-breakfast/hallway");
+        
+        AddScenery(new scenery_lamp("wall sconse", "A little sconse on the wall is enough to fill the landing with light."));
+    }
+}
+
