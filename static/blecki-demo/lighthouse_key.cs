721ae03e50c99aafd34a442878ad5505370bf824
Blecki <jm@omnisu.com>
2014-10-12 17:13:17 -0400

    Remove demo areas.

--- static/blecki-demo/lighthouse_key.cs
+++
@@ -1,9 +0,0 @@
-﻿public class lighthouse_key : RMUD.Thing
-{
-	public override void Initialize()
-	{
-		Short = "iron key";
-		Long = "This is a small, old fashioned spade-and-barrel key.";
-		Nouns.Add("IRON", "KEY");
-	}
-}
