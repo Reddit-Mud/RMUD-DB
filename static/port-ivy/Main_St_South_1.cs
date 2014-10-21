432d9d3a3cbcfe4fb0d652ef5d7c50ad212d8ffa
Aldric Giacomoni <trevoke@gmail.com>
2014-10-20 22:52:27 -0400

    Changed 'Lord Stanner' to 'Mark Stanner'

--- static/port-ivy/Main_St_South_1.cs
+++ static/port-ivy/Main_St_South_1.cs
@@ -2,9 +2,9 @@ public class Main_St_South_1 : RMUD.Room {
     public override void Initialize() {
         RoomType = RMUD.RoomType.Exterior;
         Short = "The South end of Main Street";
-        Long = "Main Street goes Northwest from here, while to the East it becomes Crescent Street. To the west, a stone arch with an oxidized bronze plaque stands here in guise of invitation to visit the Waterfront Park.";
+        Long = "Main Street goes Northwest from here, while to the East it becomes Crescent Street. To the west, a stone arch with an oxidized bronze plaque stands here in guise of invitation to visit the Waterfront Park. The cobbles are smooth";
 
-      AddScenery("The plaque's writing is hard to decipher, but thanks a Lord Stanner for a donation. A faint engraving of a once proud profile is next to the words.", "plaque", "bronze plaque", "oxidized bronze plaque");
+      AddScenery("The plaque's writing is hard to decipher, but thanks a Mark Stanner for a donation. A faint engraving of a once proud profile is next to the words.", "plaque", "bronze plaque", "oxidized bronze plaque");
 
       OpenLink(RMUD.Direction.EAST, "port-ivy/Crescent_St_Southwest_1");
       OpenLink(RMUD.Direction.WEST, "port-ivy/Waterfront_Park_2");
