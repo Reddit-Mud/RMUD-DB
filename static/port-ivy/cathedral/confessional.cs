149dfedd056c7d0e37e07f45893d4c5f8f5fa729
Aldric Giacomoni <trevoke@gmail.com>
2014-11-19 18:42:04 -0500

    fix typo

--- static/port-ivy/cathedral/confessional.cs
+++ static/port-ivy/cathedral/confessional.cs
@@ -8,7 +8,7 @@ public class confessional: RMUD.Room
         Short = "The confessional";
         Long = "This is a small enclosed space with an uncomfortable chair and a window with a closed wooden curtain through which the priest doesn't look at you when listening and absolving your sins.";
 
-		AddScenery (new scenery_lamp("candle", "The small candle offers a dum light."));
+		AddScenery (new scenery_lamp("candle", "The small candle offers a dim light."));
 
         OpenLink(RMUD.Direction.SOUTH, "port-ivy/cathedral/north_side_nef");
     }
