fb1ab5585a31b97758c7f8d39e510a0d7babd9ae
Blecki <jm@omnisu.com>
2014-12-23 10:17:08 -0500

    Removed settings.cs from database.

--- static/settings.cs
+++
@@ -1,9 +0,0 @@
-﻿public class settings : RMUD.Settings
-{
-    public override void Initialize()
-    {
-        Banner = "~~== REDDIT MUD ==~~\r\n";
-        MessageOfTheDay = "register username - Create a new account.\r\nlogin username - Log into an existing account.";
-        NewPlayerStartRoom = "port-ivy/Main_St_South_1";
-    }
-}
